929af5a2615d806a2f84a86206f151146264e01c
murotani <mu.1ok.san@gmail.com>
2020-07-21 15:56:59 +0900

    crud-api開発？フォルダ

---
+++ app/api_sample/src/main/java/jp/kobe_u/root/root_project/RootProjectApplication.java
@@ -0,0 +1,13 @@
+package jp.kobe_u.root.root_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootProjectApplication.class, args);
+	}
+
+}

3647f02e81384dabafc35b2426e804f4bca0887c
hiroto0108 <syou.151215@docomo.ne.jp>
2020-09-16 23:14:02 +0900

    save the file

--- app/HTML_bootstrap/js/shelterNavi.js
+++ app/HTML_bootstrap/js/shelterNavi.js
@@ -1,6 +1,19 @@
 const serverAddr = getServerAddr();
-let shelterlng
-let shelterlat
+var lat;
+var lng;
+var shelter={
+
+};
+var marker = {
+
+};
+var infoWindow = {
+
+};
+
+
+
+function initMap() {}
 
 function getServerAddr() {
     let e = location.href;
@@ -104,6 +117,8 @@ function getAllShelters() {
             let localGovernmentCode = shelter.local_government_code;
 
             $( '#shelters' ).append( '<tr><td>' + id + '</td><td>' + name + '</td><td>' + address + '</td><td>' + lng + '</td><td>' + lat + '</td><td>' + area + '</td><td>' + maxFamilies + '</td><td>' + contactPhone + '</td><td>' + note + '</td><td>' + localGovernmentCode + '</td></tr>' );
+        
+        
         });
     }).fail( function ( xhr ) {
         console.log( xhr );
@@ -122,21 +137,24 @@ function getShelters() {
         url: endpoint + "/" + id,
         dataType: "JSON",
     }).done( function ( data, dataType ) {
-        let shelter = data.result;
+        shelter = data.result;
         
         let id = shelter.id;
         let name = shelter.name;
         let address = shelter.address;
-        shelterlng = shelter.lng;
-        shelterlat = shelter.lat;
+        lng = shelter.lng;
+        lat = shelter.lat;
         let area = shelter.area;
         let maxFamilies = shelter.max_families;
         let contactPhone = shelter.contact_phone;
         let note = shelter.note;
         let localGovernmentCode = shelter.local_government_code;
 
-        $('result').append( '<table><tr><th scope="col">避難所ID</th><th scope="col">施設名</th><th scope="col">住所</th><th scope="col">経度</th><th scope="col">緯度</th><th scope="col">地域</th><th scope="col">収容人数</th><th scope="col">電話番号</th><th scope="col">備考</th><th scope="col">地方自治体コード</th></tr><tr><td>' + id + '</td><td>' + name + '</td><td>' + address + '</td><td>' + shelterlng + '</td><td>' + shelterlat + '</td><td>' + area + '</td><td>' + maxFamilies + '</td><td>' + contactPhone + '</td><td>' + note + '</td><td>' + localGovernmentCode + '</td></tr></table>' );
+       
+        $('result').append( '<table><tr><th scope="col">避難所ID</th><th scope="col">施設名</th><th scope="col">住所</th><th scope="col">経度</th><th scope="col">緯度</th><th scope="col">地域</th><th scope="col">収容人数</th><th scope="col">電話番号</th><th scope="col">備考</th><th scope="col">地方自治体コード</th></tr><tr><td>' + id + '</td><td>' + name + '</td><td>' + address + '</td><td>' + lng + '</td><td>' + lat + '</td><td>' + area + '</td><td>' + maxFamilies + '</td><td>' + contactPhone + '</td><td>' + note + '</td><td>' + localGovernmentCode + '</td></tr></table>' );
         
+        initMap()
+
     }).fail( function ( xhr ) {
         console.log( xhr );
         showMessage( "避難所が存在しません" );
@@ -163,25 +181,54 @@ function clearCreateForm() {
 }
 
 function initMap() {
-    {
-        var shelter = new google.maps.LatLng(shelterlat, shelterlng);
-      
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 16,
-          center: shelter
-        });
-        var marker = new google.maps.Marker({
-            position: shelter,
-            map: map
-          });
-   }
-
-
+  
+    var center = {
+        lat: +34.8394324,
+        lng: +134.693894,
+    };
+  
+    
+    var latlng = new google.maps.LatLng(parseFloat(center.lat),parseFloat(center.lng) );
+    var myOptions =  {
+        zoom: 14,
+        center: latlng,
+    };
+    map = new google.maps.Map(document.getElementById('map'),myOptions);
+    alert(latlng);
+    addHinanjo();
 }
+//避難所の追加
+function addHinanjo() {
+    for (var id in shelter) {
+        //避難所リストから1つ取り出してhとする
+        var h = shelter[id];
+        //避難所の緯度・経度
+        var pos = { lat: h.lat, lng: h.lng };
+        //h用のマーカーを作成
+        marker[id] = new google.maps.Marker({ // マーカーの追加
+            position: pos,
+            map: map, 
+            title: h.name
+        });
+        //h用の情報ウィンドウ（吹き出し）を作成
+        infoWindow[id] = new google.maps.InfoWindow({ // 吹き出しの追加
+            content: '<div class="info">' + h.name + ' ('+ h.address + ') ' + '</div>' // 吹き出しに表示する内容
+        });
+        //マーカーとウィンドウのバインド（別関数でやらないとダメ．クリック時のidが変わるので）
+        bind_info(id);
+    }
+}
+//マーカークリック時に情報ウィンドウを開く
+function bind_info(id) {
+    marker[id].addListener('click', function() { // マーカーをクリックしたとき
+        infoWindow[id].open(map, marker[id]); // 吹き出しの表示
+    });
+}
+
+
 
 function f(){
     getShelters()
-    initMap()
-
-
+    
+    
 }
